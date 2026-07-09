e7ad475a8aca8da9b2c88f5d9ca4350e54bae001
infotech <infoconwayy@gmail.com>
2026-07-09 15:05:10 +0530

    Ignore TypeScript build errors

--- project/next.config.js
+++ project/next.config.js
@@ -1,27 +1,45 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //     output: 'export',
+// //   eslint: {
+// //     ignoreDuringBuilds: true,
+// //   },
+
+// //   typescript: {
+// //     ignoreBuildErrors: true,
+// //   },
+
+// //   images: {
+// //     unoptimized: true,
+// //   },
+// // };
+
+// // module.exports = nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export',
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
